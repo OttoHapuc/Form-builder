1b41d56af8de0fcf1f389216e8abc04b42fc8b37
OttoHapuc <dossantoshapuc@gmail.com>
2024-11-01 20:35:19 -0300

    feat: page home view actions test

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,33 @@
-import HomeModel from "./model";
-import HomeView from "./view";
+import { useState } from 'react';
+
+import { SubElementsHeaderPropsType } from '../types/organisms/ElementHeaderPropsType';
+import { SimpleFormType } from '../types/simple-form';
+
+import ElementHeader from '../components/templates/ElementHeader';
+import FormPreview from '../components/templates/FormPreview';
+
+export const FORM_ELEMENTS: SubElementsHeaderPropsType[] = [
+  { type: 'section', icon: 'TbNewSection', label: 'Section' },
+  { type: 'text', icon: 'ImTextWidth', label: 'Text Input' },
+  { type: 'number', icon: 'GoNumber', label: 'Number Input' },
+  { type: 'select', icon: 'GoMultiSelect', label: 'Select' },
+  { type: 'checkbox', icon: 'IoMdCheckboxOutline', label: 'Checkbox' },
+  { type: 'radio', icon: 'IoMdRadioButtonOn', label: 'Radio' },
+  { type: 'file', icon: 'FaRegFile', label: 'File' },
+];
 
 const Home = () => {
-  const homeModel = HomeModel();
-  return <HomeView {...homeModel} />;
+  const [typeForm, setTypeForm] = useState<SimpleFormType>({
+    steps: [],
+    sections: [],
+  });
+
+  return (
+    <main className="min-h-dvh w-full relative flex flex-col bg-slate-200 overflow-hidden">
+      <ElementHeader elements={FORM_ELEMENTS} />
+      <FormPreview typeForm={typeForm} setTypeForm={setTypeForm} />
+    </main>
+  );
 };
 
 export default Home;
--- frontend/src/pages/model.tsx
+++
@@ -1,9 +0,0 @@
-const HomeModel = () => {
-  const hello = 'hello';
-
-  return {
-    hello
-  };
-};
-
-export default HomeModel;
