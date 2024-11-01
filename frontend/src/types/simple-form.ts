837d17037d688df7872920d5721684f29ee5ef60
OttoHapuc <dossantoshapuc@gmail.com>
2024-11-01 20:17:57 -0300

    feat: all types object page

--- frontend/src/types/simple-form.ts
+++ frontend/src/types/simple-form.ts
@@ -1,64 +1,58 @@
-// Tipo principal para o formulário simples
+import { WidthElementType } from '../constants/UI/width/index';
 export type SimpleFormType = {
-  steps?: SimpleFormStepType[]; // Se presente, representa um formulário com etapas
-  sections: FormSectionType[]; // Seções principais do formulário se for single-step
+  steps: SimpleFormStepType[];
+  sections: FormSectionType[];
 };
 
 export type SimpleFormStepType = {
   id: string;
-  title: string; // Título do Step
-  iconName?: string; // Nome do ícone do Step
-  sections: FormSectionType[]; // Seções contidas no Step
+  title: string;
+  iconName?: string;
+  sections: FormSectionType[];
 };
 
 export type FormSectionType = {
-  title?: string; // Título opcional da seção
-  columns: ColumnType[]; // Configuração das colunas da seção
+  title?: string;
+  fields: FormFieldType[];
 };
 
-// Tipo para a configuração de colunas dentro de cada seção
-export type ColumnType = {
-  widthPercentage: number; // Largura em %
-  fields: FormFieldType[]; // Lista de campos dentro da coluna
-};
+export type FieldInputType =
+| 'text'
+| 'number'
+| 'date'
+| 'select'
+| 'checkbox'
+| 'radio'
+| 'textarea'
+| 'file';
 
 // Tipo base para qualquer campo do formulário
 export type FormFieldType = {
-  id: string; // ID único do campo
-  label: string; // Rótulo do campo
-  fieldType: FieldInputType; // Tipo específico do campo
-  placeholder?: string; // Placeholder do campo
-  required?: boolean; // Campo obrigatório
-  readOnly?: boolean; // Somente leitura
-  disabled?: boolean; // Desabilitado (oculto apenas no CRUD view)
-  color?: string; // Cor customizada
-  triggers?: FieldTriggerType[]; // Gatilhos
-  validations?: FieldValidationType[]; // Validações
-  configOptions: FieldConfigOptions; // Configurações específicas do campo
+  id: string; // Nome da coluna na tabela pertencente
+  label: string;
+  value?: string | number | boolean | File | null | readonly string[];
+  fieldType: FieldInputType;
+  config: FieldConfig;
 };
 
-// Especificação dos tipos de input aceitos no formulário
-export type FieldInputType =
-  | 'inputText'
-  | 'inputNumber'
-  | 'date'
-  | 'select'
-  | 'checkbox'
-  | 'radio'
-  | 'textarea'
-  | 'file';
-
-// Configurações específicas de cada tipo de input
-export type FieldConfigOptions = {
-  maxLength?: number; // Para campos de texto e textarea
-  options?: SelectOptionType[]; // Opções para selects e radio
-  mask?: string; // Máscara, caso aplicável
-  customValidationRegex?: RegExp; // Regex customizada para validação
-  onActionTrigger?: () => void; // Função customizada para ação do campo
-  size?: 'small' | 'medium' | 'large'; // Tamanho de campo
+export type FieldConfig = {
+  placeholder?: string;
+  required?: boolean;
+  readOnly?: boolean;
+  disabled?: boolean;
+  color?: string;
+  maxLength?: number;
+  options?: SelectOptionType[];
+  mask?: string;
+  onChange?: (_value: string | number | boolean | File | null) => void;
+  customValidationRegex?: RegExp;
+  onActionTrigger?: () => void;
+  size?: WidthElementType;
+  block?: boolean;
+  triggers?: FieldTriggerType[];
+  validations?: FieldValidationType[];
 };
 
-// Tipo para opções de selects e radio buttons
 export type SelectOptionType = {
   label: string;
   value: string | number;
@@ -67,9 +61,9 @@ export type SelectOptionType = {
 // Gatilhos para ações e dependências do campo
 export type FieldTriggerType = {
   triggerType: 'dependency' | 'customFunction';
-  dependentFieldIds?: string[]; // IDs dos campos dependentes
-  requestUrl?: string; // URL para preenchimento dinâmico
-  actionFunction?: () => void; // Função de ação customizada
+  dependentFieldIds?: string[];
+  requestUrl?: string;
+  actionFunction?: () => void;
 };
 
 // Validações específicas para o campo
